47cf922ebca2e1c1b69b324ca317994127d38742
Avreghly Barra <avreghly@gmail.com>
2019-10-02 17:04:15 +0700

    Finish func DeleteAllFunc

--- internal/server/handlers/funcs.go
+++ internal/server/handlers/funcs.go
@@ -1,20 +1,52 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shrotavre/filetinder/internal/filetinder"
 )
 
 func delaySecond(n time.Duration) {
 	time.Sleep(n * time.Second)
 }
 
+func hasTag(ts []string, s string) bool {
+	for _, t := range ts {
+		if t == s {
+			return true
+		}
+	}
+	return false
+}
+
 // DeleteAllFunc return gin handler to delete all marked files
 func DeleteAllFunc(c *gin.Context) {
-	c.Status(http.StatusNotImplemented)
+	ts := filetinder.TargetStore
+	nts := make([]*filetinder.Target, 0)
+
+	for _, t := range ts {
+		removed := true
+
+		if hasTag(t.Tags, "remove") {
+			err := os.Remove(t.URL)
+			if err != nil {
+				log.Panic(err)
+				removed = false
+			}
+		}
+
+		if removed == false {
+			nts = append(nts, t)
+		}
+	}
+
+	ts = nts
+
+	c.Status(http.StatusOK)
 }
 
 // StopServerFunc return gin handler to stop server
